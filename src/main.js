23d1d13fb72039dda637896dcc09d44f03924b7e
shelyZhu <what_rn@163.com>
2020-04-12 15:50:27 +0800

    使用element-ui和vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// import router from './router/router'
+// @是一个路径的别名  src路径  在vue-cli 项目下有效
+// router.js 是目录下的索引文件，默认使用的索引文件
+import router from '@/router'
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+
 new Vue({
-  render: h => h(App)
+  // 是一个vue的配置项 指向的是函数
+  // render 的优先级别最高 相比el template
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,7 @@
+import vue from 'vue'
+import VueRouter from 'vue-router'
+vue.use(VueRouter)
+var router = new VueRouter({
+  routes: []
+})
+export default router
